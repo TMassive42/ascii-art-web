0a0156e74592c686eae14a02f247ca8a720a5aeb
TMassive42 <obimbira60@gmail.com>
2024-07-09 15:13:09 +0300

    create server package

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strings"
-	"asciiartserver/asciiart"
+	server "asciiartserver/server"
 )
 
-var tmpl *template.Template
-type PageData struct {
-	Art   string
-	Error string
-}
+// var Tmpl *template.Template
+
+// type PageData struct {
+// 	Art   string
+// 	Error string
+// }
 
 func main() {
 
 	var err error
 
 	// Parse the template file
-	tmpl, err = template.ParseFiles("templates/index.html")
+	server.Tmpl, err = template.ParseFiles("templates/index.html")
 	if err != nil {
 		log.Printf("Error parsing template: %v", err)
 	}
@@ -29,7 +29,7 @@ func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		// Handle valid paths
 		if r.URL.Path == "/" {
-			asciiArtHandler(w, r)
+			server.AsciiArtHandler(w, r)
 			return
 		}
 	
@@ -53,61 +53,3 @@ func main() {
 		log.Fatalf("Error starting server: %v", err)
 	}
 }
-
-func renderTemplate(w http.ResponseWriter, data *PageData) {
-	if tmpl == nil {
-		log.Println("Template file not found")
-		http.Error(w, "Template file not found", http.StatusNotFound)
-		return
-	}
-	if err := tmpl.Execute(w, data); err != nil {
-		log.Printf("Error executing template: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	} else {
-		log.Println("Template executed successfully")
-	}
-}
-
-func handleError(w http.ResponseWriter, data *PageData, statusCode int, errMsg string, logMsg string) {
-	data.Error = errMsg
-	log.Println(logMsg)
-	// Set the status code here
-	w.WriteHeader(statusCode)
-	// Render the template after setting the status code
-	renderTemplate(w, data)
-}
-
-func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		// If not a POST request, just render the form
-		data := &PageData{}
-		renderTemplate(w, data)
-		return
-	}
-
-	input := r.FormValue("input")
-	banner := "asciiart/banners/" + r.FormValue("banner")
-
-	data := &PageData{}
-	if input == "" || banner == "" {
-		handleError(w, data, http.StatusBadRequest, "Both text input and banner selection are required.", "Error: Missing input or banner selection")
-		return
-	}
-
-	art, err := asciiart.GenerateASCIIArt(input, banner)
-	if err != nil {
-		switch err {
-		case asciiart.ErrNotFound:
-			handleError(w, data, http.StatusNotFound, "The specified banner was not found.", fmt.Sprintf("Error: %v", err))
-		case asciiart.ErrBadRequest:
-			handleError(w, data, http.StatusBadRequest, "The request was incorrect. Please check your input.", fmt.Sprintf("Error: %v", err))
-		default:
-			handleError(w, data, http.StatusInternalServerError, "An internal error occurred. Please try again later.", fmt.Sprintf("Internal error: %v", err))
-		}
-		return
-	}
-
-	data.Art = art
-	renderTemplate(w, data)
-}
-
