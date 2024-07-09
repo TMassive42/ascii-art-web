3af06a21c21d0da6248afe8cea2bb23a304bedb1
TMassive42 <obimbira60@gmail.com>
2024-07-09 12:35:13 +0300

    changed module name

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"asciiartserver/server"
+	"asciiartserver/asciiart"
 )
+
+var tmpl *template.Template
+type PageData struct {
+	Art   string
+	Error string
+}
 
 func main() {
 
@@ -18,7 +25,7 @@ func main() {
 	}
 
 	// Define the handler function for the root path
-	http.HandleFunc("/", server.AsciiArtHandler)
+	http.HandleFunc("/", asciiArtHandler)
 
 	// Serve other static files (e.g., CSS, JS) using FileServer
 	staticDir := http.Dir("static")
@@ -31,4 +38,60 @@ func main() {
 	}
 }
 
-var tmpl *template.Template
+func renderTemplate(w http.ResponseWriter, data *PageData) {
+	if tmpl == nil {
+		log.Println("Template file not found")
+		http.Error(w, "Template file not found", http.StatusNotFound)
+		return
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	} else {
+		log.Println("Template executed successfully")
+	}
+}
+
+func handleError(w http.ResponseWriter, data *PageData, statusCode int, errMsg string, logMsg string) {
+	data.Error = errMsg
+	log.Println(logMsg)
+	// Set the status code here
+	w.WriteHeader(statusCode)
+	// Render the template after setting the status code
+	renderTemplate(w, data)
+}
+
+func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// If not a POST request, just render the form
+		data := &PageData{}
+		renderTemplate(w, data)
+		return
+	}
+
+	input := r.FormValue("input")
+	banner := r.FormValue("banner")
+
+	data := &PageData{}
+	if input == "" || banner == "" {
+		handleError(w, data, http.StatusBadRequest, "Both text input and banner selection are required.", "Error: Missing input or banner selection")
+		return
+	}
+
+	art, err := asciiart.GenerateASCIIArt(input, banner)
+	if err != nil {
+		switch err {
+		case asciiart.ErrNotFound:
+			handleError(w, data, http.StatusNotFound, "The specified banner was not found.", fmt.Sprintf("Error: %v", err))
+		case asciiart.ErrBadRequest:
+			handleError(w, data, http.StatusBadRequest, "The request was incorrect. Please check your input.", fmt.Sprintf("Error: %v", err))
+		default:
+			handleError(w, data, http.StatusInternalServerError, "An internal error occurred. Please try again later.", fmt.Sprintf("Internal error: %v", err))
+		}
+		return
+	}
+
+	data.Art = art
+	renderTemplate(w, data)
+}
+
